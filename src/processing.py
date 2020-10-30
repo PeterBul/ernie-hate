a936d02203923e0d789dd70529e87403079ca03e
PeterBul <pc.bulukin@gmail.com>
2020-10-30 14:05:08 +0100

    Changed processing to function

--- src/processing.py
+++ src/processing.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 mapping = {0:1, 1:0, 2:2, 3:2}
 
-df = pd.read_csv('../data/founta/test.tsv', sep='\t')
+def convert_dataset(ds):
+  df = pd.read_csv('../data/founta/{}.tsv'.format(ds), sep='\t')
 
-df.label = df.label.apply(lambda l: mapping[l])
+  df.label = df.label.apply(lambda l: mapping[l])
 
-print(df.head())
+  print(df.head())
 
-df.to_csv('../data/founta/test-conv.tsv', sep='\t')
+  df.to_csv('../data/founta/conv/{}.tsv'.format(ds), sep='\t')
+
+convert_dataset('train')
+convert_dataset('dev')
