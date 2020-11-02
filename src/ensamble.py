8ed6fe0c0033c0542250ecbd6f2d6fb100d9b4dd
Peter Cook Bulukin <pc.bulukin@gmail.com>
2020-11-02 14:33:13 +0100

    Use archive in ensamble

--- src/ensamble.py
+++ src/ensamble.py
@@ -6,28 +6,42 @@ from print_metrics import print_metrics
 
 ERNIE_PATH = '../ERNIE/'
 ENSAMBLE_PATH = '../configs/ensamble.json'
-dataset = 'founta'
+ARCHIVE_PATH = '../configs/archive.json'
+dataset = 'solid'
 #target_names = ['Hateful', 'Offensive', 'Neither']
 target_names = ['Abusive', 'Hateful', 'Normal', 'Spam']
+target_names = ['OFF', 'NOT']
 
+with open(ARCHIVE_PATH, 'r') as f:
+  text = f.read()
+  archive = json.loads(text)[dataset]
+
+file_paths = [archive[model_size][lr]['results'] for model_size in ['base', 'large'] for lr in ['1e-05', '2e-05']]
+
+"""
 with open(ENSAMBLE_PATH, 'r') as f:
   text = f.read()
   file_paths = json.loads(text)[dataset]
+"""
+
 
 preds = []
 probs = []
 
-for path in file_paths:
+for i, path in enumerate(file_paths):
   df = pd.read_csv(os.path.join(ERNIE_PATH, path), sep="\t", names=['preds', 'probs'])
   df.probs = df.probs.apply(lambda prob: np.array([float(p) for p in prob[1:-1].split()])) 
-
   probs_tmp = np.stack(df.probs, axis=0)
+  probs_tmp = probs_tmp[:,:2]
   print(probs_tmp.dtype)
   probs.append(probs_tmp)
   preds.append(df.preds.to_numpy(dtype=np.int32))
 
 y_maj = [np.argmax(np.bincount(p)) for p in np.transpose(np.asarray(preds))]
-y_sum = np.argmax(np.sum(np.asarray(probs), axis=0), axis=-1)
+for prob in probs:
+  print(prob.shape)
+tmp = np.asarray(probs)
+y_sum = np.argmax(np.sum(tmp, axis=0), axis=-1)
 test_df = pd.read_csv(os.path.join('../data', dataset, 'test.tsv'), sep='\t')
 y_true = test_df.label.to_numpy()
 
