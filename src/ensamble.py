e17dd1b49baa26d135b6074a17bfda593fb88b09
PeterBul <pc.bulukin@gmail.com>
2020-11-03 17:55:34 +0100

    Merge ensamble.py

--- src/ensamble.py
+++ src/ensamble.py
@@ -7,17 +7,35 @@ from print_metrics import print_metrics
 ERNIE_PATH = '../ERNIE/'
 ENSAMBLE_PATH = '../configs/ensamble.json'
 ARCHIVE_PATH = '../configs/archive.json'
-model_dataset = 'davidson-founta/conv'
-test_dataset = 'founta/conv'
+
 target_names = ['Hateful', 'Offensive', 'Neither']
 #target_names = ['Abusive', 'Hateful', 'Normal', 'Spam']
 #target_names = ['OFF', 'NOT']
 
-with open(ARCHIVE_PATH, 'r') as f:
-  text = f.read()
-  archive = json.loads(text)[model_dataset]
 
-file_paths = [archive[model_size][lr]['results'] for model_size in ['base', 'large'] for lr in ['1e-05', '2e-05']]
+def get_file_paths(dataset):
+  with open(ARCHIVE_PATH, 'r') as f:
+    text = f.read()
+    archive = json.loads(text)[dataset]
+
+  return [archive[model_size][lr]['results'] for model_size in ['base', 'large'] for lr in ['1e-05', '2e-05']]
+
+def get_preds_and_probs(dataset):
+  file_paths = get_file_paths(dataset)
+  preds = []
+  probs = []
+
+  for path in file_paths:
+    df = pd.read_csv(os.path.join(ERNIE_PATH, path), sep="\t", names=['preds', 'probs'])
+    df.probs = df.probs.apply(lambda prob: np.array([float(p) for p in prob[1:-1].split()])) 
+    probs_tmp = np.stack(df.probs, axis=0)
+    probs_tmp = probs_tmp[:,:2]
+    probs.append(probs_tmp)
+    preds.append(df.preds.to_numpy(dtype=np.int32))
+
+  return preds, probs
+
+
 
 """
 with open(ENSAMBLE_PATH, 'r') as f:
@@ -26,28 +44,32 @@ with open(ENSAMBLE_PATH, 'r') as f:
 """
 
 
-preds = []
-probs = []
+def get_majority_voting(preds):
+  return [np.argmax(np.bincount(p)) for p in np.transpose(np.asarray(preds))]
+  
+def get_sum_of_probs(probs):
+  tmp = np.asarray(probs)
+  return np.argmax(np.sum(tmp, axis=0), axis=-1)
 
-for i, path in enumerate(file_paths):
-  df = pd.read_csv(os.path.join(ERNIE_PATH, path), sep="\t", names=['preds', 'probs'])
-  df.probs = df.probs.apply(lambda prob: np.array([float(p) for p in prob[1:-1].split()])) 
-  probs_tmp = np.stack(df.probs, axis=0)
-  probs_tmp = probs_tmp[:,:3]
-  print(probs_tmp.dtype)
-  probs.append(probs_tmp)
-  preds.append(df.preds.to_numpy(dtype=np.int32))
+def get_y_true(dataset):
+  test_df = pd.read_csv(os.path.join('../data', dataset, 'test.tsv'), sep='\t')
+  return test_df.label.to_numpy()
 
-y_maj = [np.argmax(np.bincount(p)) for p in np.transpose(np.asarray(preds))]
-for prob in probs:
-  print(prob.shape)
-tmp = np.asarray(probs)
-y_sum = np.argmax(np.sum(tmp, axis=0), axis=-1)
-test_df = pd.read_csv(os.path.join('../data', test_dataset, 'test.tsv'), sep='\t')
-y_true = test_df.label.to_numpy()
 
-print("-------------Majority voting---------------")
-print_metrics(y_true, y_maj, target_names)
 
-print('-----------Sum of probabilities------------')
-print_metrics(y_true, y_sum, target_names)
+
+if __name__ == "__main__":
+  model_dataset = 'davidson-founta/conv'
+  test_dataset = 'founta/conv'
+  preds, probs = get_preds_and_probs(model_dataset)
+
+  y_maj = get_majority_voting(preds)
+  y_sum = get_sum_of_probs(probs)
+  y_true = get_y_true(test_dataset)
+
+
+  print("-------------Majority voting---------------")
+  print_metrics(y_true, y_maj, target_names)
+
+  print('-----------Sum of probabilities------------')
+  print_metrics(y_true, y_sum, target_names)
