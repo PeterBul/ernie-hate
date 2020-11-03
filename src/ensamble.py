7ccfc07025e516603faa9b8c3c6967cff1adeaf7
Peter Cook Bulukin <pc.bulukin@gmail.com>
2020-11-03 17:42:28 +0100

    Change evaluation to use test sets

--- src/ensamble.py
+++ src/ensamble.py
@@ -7,14 +7,15 @@ from print_metrics import print_metrics
 ERNIE_PATH = '../ERNIE/'
 ENSAMBLE_PATH = '../configs/ensamble.json'
 ARCHIVE_PATH = '../configs/archive.json'
-dataset = 'solid'
-#target_names = ['Hateful', 'Offensive', 'Neither']
-target_names = ['Abusive', 'Hateful', 'Normal', 'Spam']
-target_names = ['OFF', 'NOT']
+model_dataset = 'davidson-founta/conv'
+test_dataset = 'founta/conv'
+target_names = ['Hateful', 'Offensive', 'Neither']
+#target_names = ['Abusive', 'Hateful', 'Normal', 'Spam']
+#target_names = ['OFF', 'NOT']
 
 with open(ARCHIVE_PATH, 'r') as f:
   text = f.read()
-  archive = json.loads(text)[dataset]
+  archive = json.loads(text)[model_dataset]
 
 file_paths = [archive[model_size][lr]['results'] for model_size in ['base', 'large'] for lr in ['1e-05', '2e-05']]
 
@@ -32,7 +33,7 @@ for i, path in enumerate(file_paths):
   df = pd.read_csv(os.path.join(ERNIE_PATH, path), sep="\t", names=['preds', 'probs'])
   df.probs = df.probs.apply(lambda prob: np.array([float(p) for p in prob[1:-1].split()])) 
   probs_tmp = np.stack(df.probs, axis=0)
-  probs_tmp = probs_tmp[:,:2]
+  probs_tmp = probs_tmp[:,:3]
   print(probs_tmp.dtype)
   probs.append(probs_tmp)
   preds.append(df.preds.to_numpy(dtype=np.int32))
@@ -42,7 +43,7 @@ for prob in probs:
   print(prob.shape)
 tmp = np.asarray(probs)
 y_sum = np.argmax(np.sum(tmp, axis=0), axis=-1)
-test_df = pd.read_csv(os.path.join('../data', dataset, 'test.tsv'), sep='\t')
+test_df = pd.read_csv(os.path.join('../data', test_dataset, 'test.tsv'), sep='\t')
 y_true = test_df.label.to_numpy()
 
 print("-------------Majority voting---------------")
--- src/evaluation.py
+++ src/evaluation.py
@@ -16,9 +16,11 @@ args = parser.parse_args()
 
 ERNIE_PATH = '../ERNIE/'
 archive_path = '../configs/archive.json'
-dataset = 'founta/conv'
+model_dataset = 'founta/conv'
+test_dataset = 'davidson'
 #target_names = ['NOT', 'OFF']
 target_names = ['Hateful', 'Offensive', 'Neither']
+#target_names = ['Hateful', 'Offensive']
 #target_names = ['Abusive', 'Hateful', 'Normal', 'Spam']
 
 founta2davidson = {0:1, 1:0, 2:2, 3:2}
@@ -27,13 +29,13 @@ def evaluate(model_size, lr_text):
   with open(archive_path, 'r') as f:
     text = f.read()
     archive = json.loads(text)
-  path = args.results if args.results else archive[dataset][model_size][lr_text]['results']
+  path = args.results if args.results else archive[model_dataset][test_dataset][model_size][lr_text]['results']
   df = pd.read_csv(os.path.join(ERNIE_PATH, path), sep='\t', names=['preds', 'probs'])
   if args.founta2davidson:
     df.preds = df.preds.apply(lambda p: founta2davidson[p])
   
   preds = df.preds.to_numpy(dtype=np.int32)
-  test_df = pd.read_csv(os.path.join('../data', dataset, 'test.tsv'), sep='\t')
+  test_df = pd.read_csv(os.path.join('../data', test_dataset, 'test.tsv'), sep='\t')
   y_true = test_df.label.to_numpy()
 
   print_metrics(y_true, preds, target_names)
