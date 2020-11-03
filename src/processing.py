f5c229db4a100be454eef136252f71bd21fd2559
Peter Cook Bulukin <pc.bulukin@gmail.com>
2020-11-03 17:42:57 +0100

    Add oversample

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from tqdm import tqdm
 
 mapping = {0:1, 1:0, 2:2, 3:2}
 
@@ -19,4 +20,18 @@ def remove_neither(ds):
 
   df.to_csv('../data/davidson/filtered/{}.tsv'.format(ds), sep='\t', index=False)
 
-remove_neither('train')
+def oversample(ds):
+  df = pd.read_csv('../data/davidson/filtered/{}.tsv'.format(ds), sep='\t')
+  new_df = pd.DataFrame({'text_a':[], 'label': []})
+  for i, row, in tqdm(df.iterrows(), total=df.shape[0]):
+    new_df = new_df.append(row, ignore_index=True)
+    if row.label == 0:
+      for i in range(7):
+        new_df = new_df.append(row, ignore_index=True)
+  new_df.label = new_df.label.astype(int)
+  new_df = new_df.sample(frac=1, random_state=42)
+  new_df.to_csv('../data/davidson/filtered/{}-oversampled.tsv'.format(ds), sep='\t', index=False)
+
+
+oversample('train-full')
+
